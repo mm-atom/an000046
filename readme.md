9bd920dd8866407de5368afbef8b2bd85b5cce1b
taoqf <tao_qiufeng@126.com>
2022-09-26 09:10:39 +0800

    加密

--- readme.md
+++ readme.md
@@ -4,37 +4,9 @@
 
 ## 配置文件
 
-mm.json
+.env or .env.local
 
-```json
-{
-	"minio": {
-		"endPoint": "127.0.0.1",
-		"port": 9000,
-		"accessKey": "mmstudio",
-		"secretKey": "Mmstudio123",
-		"useSSL": false,
-		"region": "cn-north-1",
-		"partSize": 5242880
-	}
-}
+```conf
+MINIO_NAME_SPACE: mmstudio
+MINIO_CONFIG = {"endPoint":"127.0.0.1","port":9000,"accessKey":"mmstudio","secretKey":"Mmstudio123","useSSL":false,"region":"cn-north-1","partSize":5242880}
 ```
-
-## docker
-
-```yml
-version: '3.7'
-
-services:
-  minio:
-    image: minio/minio
-    container_name: minio
-    command: server /data
-    volumes:
-      - /home/taoqf/data/minio:/data
-    ports:
-      - 9000:9000
-    environment:
-      MINIO_ACCESS_KEY: mmstudio
-      MINIO_SECRET_KEY: Mmstudio123
-```%
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { readFile, rm, stat, writeFile } from 'fs/promises';
+import { createReadStream } from 'fs';
 import anylogger from 'anylogger';
-import config from '@mmstudio/config';
 import { Client, ClientOptions } from 'minio';
 import range_parser from 'range-parser';
+import an61 from '@mmstudio/an000061';
 
-const client = new Client(config.minio as ClientOptions);
 const logger = anylogger('@mmstudio/an000046');
-const NAME_SPACE = 'mmstudio';
+let gClient: Client;
 
 interface IMetaData {
 	originialfilename: string;
@@ -17,7 +18,8 @@ export default async function an46(
 	id: string,
 	download: string | 'true' | 'false' | boolean,
 	req: IncomingMessage,
-	res: ServerResponse
+	res: ServerResponse,
+	encrypt: boolean
 ) {
 	if (!id) {
 		logger.debug('method: getfile,id is empty');
@@ -26,9 +28,11 @@ export default async function an46(
 	logger.debug('Request headers:', JSON.stringify(req.headers));
 	const none_match = req.headers['if-none-match'];
 	logger.debug(`method: getfile,file_id:${id}`);
-	const stat = await client.statObject(NAME_SPACE, id);
+	const namespace = getNameSpace();
+	const client = getClient();
+	const itemStat = await client.statObject(namespace, id);
 	// Etag标识
-	const etag = `W/"${stat.etag}"`;
+	const etag = `W/"${itemStat.etag}"`;
 	res.setHeader('Etag', etag);
 	// 增加Etag判断文件是否有变动
 	if (none_match && none_match === etag) {
@@ -42,7 +46,7 @@ export default async function an46(
 	}
 	res.statusCode = 200;
 
-	const meta = stat.metaData as IMetaData;
+	const meta = itemStat.metaData as IMetaData;
 	res.setHeader('Content-Type', meta['content-type']);
 	const filename = meta.originialfilename;
 	if (download !== undefined && download !== false && download !== 'false') {
@@ -60,22 +64,36 @@ export default async function an46(
 	const r = req.headers['range'];
 	if (r) {
 		logger.info(`method: getfile,id:${id} with range:${r}`);
-		const ranges = range_parser(stat.size, r, { combine: true });
+		const ranges = range_parser(itemStat.size, r, { combine: true });
 		logger.debug(`parsed range:${JSON.stringify(ranges)}`);
 		if (ranges === -1) {
-			res.setHeader('Content-Range', `*/${stat.size}`);
+			if (encrypt) {
+				const tmp = `/tmp/${id}`;
+				await client.fGetObject(namespace, id, tmp);
+				const buf = await readFile(tmp);
+				an61.decrypt(buf);
+				await writeFile(tmp, an61.decrypt(buf));
+				const s = await stat(tmp);
+				await rm(tmp);
+				res.setHeader('Content-Range', `*/${s.size}`);
+			} else {
+				res.setHeader('Content-Range', `*/${itemStat.size}`);
+			}
 			throw new Error('Incorrect request!');
 		} else if (ranges === -2) {
 			throw new Error('Incorrect request!');
 		} else {
+			if (encrypt) {
+				throw new Error('Incorrect request!');
+			}
 			const range = ranges[0];
 			const start = range.start;
 			const end = range.end; // for lastest byte
 			res.statusCode = 206;
-			res.setHeader('Content-Range', `bytes ${start}-${end}/${stat.size}`);
+			res.setHeader('Content-Range', `bytes ${start}-${end}/${itemStat.size}`);
 			res.setHeader('Content-Length', end + 1 - start);
 			const stream = await client.getPartialObject(
-				NAME_SPACE,
+				namespace,
 				id,
 				start,
 				end - start + 1
@@ -84,7 +102,39 @@ export default async function an46(
 		}
 	} else {
 		logger.debug(`method: getfile,id:${id} without range.`);
-		const stream = await client.getObject(NAME_SPACE, id);
-		stream.pipe(res);
+		if (encrypt) {
+			const tmp = `/tmp/${id}`;
+			await client.fGetObject(namespace, id, tmp);
+			const buf = await readFile(tmp);
+			an61.decrypt(buf);
+			await writeFile(tmp, an61.decrypt(buf));
+			const fs = createReadStream(tmp);
+			fs.on('close', async () => {
+				await rm(tmp);
+			});
+			fs.pipe(res);
+		} else {
+			const stream = await client.getObject(namespace, id);
+			stream.pipe(res);
+		}
 	}
+}
+
+function getClient() {
+	if (!gClient) {
+		gClient = new Client(getConfig());
+	}
+	return gClient;
+}
+
+function getNameSpace() {
+	return process.env.MINIO_NAME_SPACE || 'mmstudio';
+}
+
+let gConfig: ClientOptions;
+function getConfig() {
+	if (!gConfig) {
+		gConfig = JSON.parse(process.env.MINIO_CONFIG!) as ClientOptions;
+	}
+	return gConfig;
 }
